f096c26737e6554b1e7aa40a9d427159e2e2ee8e
davidrf88 <davidrf88@gmail.com>
2021-10-08 02:15:11 -0400

    adding logic functionality

--- pages/login.vue
+++ pages/login.vue
@@ -13,9 +13,10 @@
             placeholder="Enter email"
           />
           <div class="inputValidationErrors">
-              <div class="form-text" v-if=" $v.email.$dirty && !$v.email.email">invalid email</div>
+            <div class="form-text" v-if="$v.email.$dirty && !$v.email.email">
+              invalid email
+            </div>
           </div>
-          
         </div>
         <div class="form-group">
           <label for="password">Password</label>
@@ -28,21 +29,24 @@
             placeholder="Password"
           />
           <div class="inputValidationErrors">
-              <div class="form-text" v-if=" $v.password.$dirty && !$v.password.minLength">password must be at least {{$v.password.$params.minLength.min}} characters long</div>
+            <div
+              class="form-text"
+              v-if="$v.password.$dirty && !$v.password.minLength"
+            >
+              password must be at least
+              {{ $v.password.$params.minLength.min }} characters long
+            </div>
           </div>
         </div>
-        <div v-if="errorMessage"  class="form-text text-info">
-            invalid form
+        <div v-if="errorMessage" class="form-text text-info">
+          {{errorMessage}}
         </div>
         <div class="text-center">
-          <button type="submit" class="btn btn-primary mt-4">Submit</button>
+          <button v-if="!inProgress" type="submit" class="btn btn-primary mt-4">Submit</button>
+          <button v-if="inProgress" type="submit" class="btn btn-primary disabled mt-4">processing...</button>
         </div>
-
       </form>
     </div>
-
-
-   
   </div>
 </template>
 
@@ -51,9 +55,10 @@ import { required, email, minLength } from "vuelidate/lib/validators";
 export default {
   data() {
     return {
-      email: "",
-      password: "",
-      errorMessage:null,
+      email: "davidrf88@gmail.com",
+      password: "password",
+      errorMessage: null,
+      inProgress: false,
     };
   },
 
@@ -67,7 +72,6 @@ export default {
       required,
       minLength: minLength(5),
     },
-
   },
 
   methods: {
@@ -80,36 +84,57 @@ export default {
       };
     },
 
-    Access() {
-        console.log('method triggered', this.$v.$invalid);
-         this.errorMessage = null;
-        if(this.$v.$invalid){ 
-            this.$v.$touch()
-            this.errorMessage = "invalid form";
-
-            return
+    async Access() {
+      this.inProgress = true;
+      this.errorMessage = null;
+      try {
+        if (this.$v.$invalid) {
+          this.$v.$touch();
+          this.errorMessage = "invalid form";
+          return;
         }
 
-      console.log(this.email, this.password);
+        await this.$store
+          .dispatch("auth/login", {
+            email: this.email,
+            password: this.password,
+          })
+          .then((response) => {
+            if (response.success && this.$store.getters["auth/IsAuthenticated"]) {
+              //redirect to user profile
+              this.$router.push("/");
+              
+            } else {
+              //display error
+              this.errorMessage = response.message;
+            }
+          });
+
+
+      } catch (err) {
+        this.errorMessage = err.message;
+      } finally {
+        this.inProgress = false;
+      }
+
+        return;
+      }
+
     },
-  },
+  
 };
 </script>
 
 <style scoped>
 .error {
-    border-color: red;
-    background: #FDD;
-  }
-  
-  .error:focus {
-    outline-color: #F99;
-  }
-  .inputValidationErrors
-  {
-      color: #17a2b8;
-  }
+  border-color: red;
+  background: #fdd;
+}
 
-
-
+.error:focus {
+  outline-color: #f99;
+}
+.inputValidationErrors {
+  color: #17a2b8;
+}
 </style>
--- store/auth.js
+++ store/auth.js
@@ -1,38 +1,95 @@
+import jwtDecode from 'jwt-decode'
 export const state = () => ({
-    user: null,
-  })
+  user: null,
+})
+
+export const getters = {
+  IsAuthenticated: state => {
+    return (state.user != null);
+  },
+  getUser: state => {
+    return state.user;
+  },
 
 
-  
-  export const actions = {
 
-    async register(context, { email, password }) {
-       
-      try {
-        const body = JSON.stringify({ email: email, password: password })
-        //Send request
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json'},
-            body: body
-//
-        };
-        const response = await fetch( process.env.APIEndpoint+'auth/register', requestOptions);
-        
-        return response.text().then(text => {
-            return text && JSON.parse(text);
-            });
+}
 
+export const mutations = {
+  setUser(state, { jwt }) {
+     
+    var decodedtkn = jwtDecode(jwt);
+    state.user = decodedtkn;
+    state.user.jwt = jwt;
+  },
+}
+
+
+
+export const actions = {
+
+  async register(context, { email, password }) {
+
+    try {
+      const body = JSON.stringify({ email: email, password: password })
+      //Send request
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: body
+        //
+      };
+      const response = await fetch(process.env.APIEndpoint + 'auth/register', requestOptions);
+
+      return response.text().then(text => {
+        return text && JSON.parse(text);
+      });
+
+    }
+    catch (error) {
+
+      return { success: false, message: 'Service unavailable - ' + error.message };
+    }
+
+
+
+
+
+
+  },
+
+  async login(context, { email, password }) {
+
+    try {
+      const body = JSON.stringify({ email: email, password: password })
+      //Send request
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: body
+        //
+      };
+      const response = await fetch(process.env.APIEndpoint + 'auth/login', requestOptions);
+       var resp = await response.text().then(text => {
+        return text && JSON.parse(text);
+      }); 
+    
+      if(!resp.success)
+      {
+        return { success: false, message:  resp.message };
       }
-      catch (error) {
-        
-        return {success: false, message: 'Service unavailable - ' + error.message};
-      }
-  
-  
-  
-  
-  
-  
-    },
+
+      context.commit("setUser", { jwt: resp.jwt });
+
+      return resp
+
+    }
+    catch (error) {
+
+      return { success: false, message: 'Service unavailable - ' + error.message };
+    }
+
+
+  },
+
 }
